a249d061817b028f7666a45fe9cd459219eb1297
Shinigamiiz <119001740+Shinigamiiz@users.noreply.github.com>
2022-11-27 14:21:03 +0530

    Update config.py

--- NekoRobot/config.py
+++ NekoRobot/config.py
@@ -34,7 +34,7 @@ class Config(object):
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = "postgres://pdepdgek:YLC10ZEJAjCC23wVGoNaL2qPsKMcsO6x@lucky.db.elephantsql.com/pdepdgek"  # needed for any database modules
-    DB_URL = "postgres://owrivopvbczaip:8c24f469ed21a5db5fccd405143072aafb5d902753ef2b6ed5e7395224fc36b1@ec2-18-206-103-49.compute-1.amazonaws.com:5432/da5jikk46tbn3i"
+    DB_URL = "postgres://bfkskrbbdynjrg:834c5e7774030cb4c00bea8049d32b96d555ae0b3078a751b243adc31e3d5ed5@ec2-3-89-230-115.compute-1.amazonaws.com:5432/dfp627imqb2maa"
     REDIS_URL = "redis://default:6vsajwd6wq3lNI7T3zMIpltTrU040DJC@redis-19248.c16.us-east-1-3.ec2.cloud.redislabs.com:19248"
     LOAD = []
     ARQ_API_KEY = "arq.hamker.dev"
