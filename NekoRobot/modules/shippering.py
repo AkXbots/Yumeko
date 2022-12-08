1277917d78e6e1c3f081e57ca4423f5e58f5de7a
Shinigamiiz <119001740+Shinigamiiz@users.noreply.github.com>
2022-12-09 04:28:36 +0530

    Update shippering.py

--- NekoRobot/modules/shippering.py
+++ NekoRobot/modules/shippering.py
@@ -5,7 +5,7 @@ from pyrogram import filters
 from pyrogram.enums import ChatType
 
 from NekoRobot import pgram as pbot
-from NekoRobot.modules.mongo import get_couple, save_couple
+from NekoRobot.modules.mongo.couples_mongo import get_couple, save_couple
 
 
 # Date and time
