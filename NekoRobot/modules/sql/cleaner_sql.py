60d3ea248edc9049940c2d6af4915fd82ed6c81d
Shinigamiiz <119001740+Shinigamiiz@users.noreply.github.com>
2022-12-09 03:52:03 +0530

    Update cleaner_sql.py

--- NekoRobot/modules/sql/cleaner_sql.py
+++ NekoRobot/modules/sql/cleaner_sql.py
@@ -1,28 +1,3 @@
-"""
-BSD 2-Clause License
-Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince]
-Copyright (c) 2022-2023, Programmer Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
-All rights reserved.
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
 import threading
 
 from sqlalchemy import Boolean, Column, UnicodeText
@@ -76,20 +51,12 @@ GLOBAL_IGNORE_COMMANDS = set()
 def set_cleanbt(chat_id, is_enable):
     with CLEANER_CHAT_SETTINGS:
         curr = SESSION.query(CleanerBlueTextChatSettings).get(str(chat_id))
+        if curr:
+            SESSION.delete(curr)
 
-        if not curr:
-            curr = CleanerBlueTextChatSettings(str(chat_id), is_enable)
-        else:
-            curr.is_enabled = is_enable
+        newcurr = CleanerBlueTextChatSettings(str(chat_id), is_enable)
 
-        if str(chat_id) not in CLEANER_CHATS:
-            CLEANER_CHATS.setdefault(
-                str(chat_id), {"setting": False, "commands": set()}
-            )
-
-        CLEANER_CHATS[str(chat_id)]["setting"] = is_enable
-
-        SESSION.add(curr)
+        SESSION.add(newcurr)
         SESSION.commit()
 
 
@@ -102,7 +69,8 @@ def chat_ignore_command(chat_id, ignore):
 
             if str(chat_id) not in CLEANER_CHATS:
                 CLEANER_CHATS.setdefault(
-                    str(chat_id), {"setting": False, "commands": set()}
+                    str(chat_id),
+                    {"setting": False, "commands": set()},
                 )
 
             CLEANER_CHATS[str(chat_id)]["commands"].add(ignore)
@@ -124,7 +92,8 @@ def chat_unignore_command(chat_id, unignore):
 
             if str(chat_id) not in CLEANER_CHATS:
                 CLEANER_CHATS.setdefault(
-                    str(chat_id), {"setting": False, "commands": set()}
+                    str(chat_id),
+                    {"setting": False, "commands": set()},
                 )
             if unignore in CLEANER_CHATS.get(str(chat_id)).get("commands"):
                 CLEANER_CHATS[str(chat_id)]["commands"].remove(unignore)
@@ -183,11 +152,13 @@ def is_command_ignored(chat_id, command):
 
 
 def is_enabled(chat_id):
-    if str(chat_id) in CLEANER_CHATS:
-        settings = CLEANER_CHATS.get(str(chat_id)).get("setting")
-        return settings
-
-    return False
+    try:
+        resultcurr = SESSION.query(CleanerBlueTextChatSettings).get(str(chat_id))
+        if resultcurr:
+            return resultcurr.is_enable
+        return False  # default
+    finally:
+        SESSION.close()
 
 
 def get_all_ignored(chat_id):
@@ -205,7 +176,7 @@ def __load_cleaner_list():
 
     try:
         GLOBAL_IGNORE_COMMANDS = {
-            int(x.command) for x in SESSION.query(CleanerBlueTextGlobal).all()
+            (x.command) for x in SESSION.query(CleanerBlueTextGlobal).all()
         }
     finally:
         SESSION.close()
