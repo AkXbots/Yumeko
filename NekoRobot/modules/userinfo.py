590e27bf0a20b49ecee344933dc2d26d94b8cf18
ryuk04 <122408850+ryuk04@users.noreply.github.com>
2023-01-13 22:49:36 +0530

    Update userinfo.py

--- NekoRobot/modules/userinfo.py
+++ NekoRobot/modules/userinfo.py
@@ -45,7 +45,7 @@ from NekoRobot.modules.sql.afk_sql import check_afk_status, is_afk
 from NekoRobot.modules.sql.global_bans_sql import is_user_gbanned
 from NekoRobot.modules.sql.users_sql import get_user_num_chats
 
-NEKO_IMG = "https://graph.org/file/288e6b9a0e2ebb1ac3017.jpg"
+NEKO_IMG = "https://graph.org/file/25054da79f8cfd95fbf98.jpg"
 
 
 def no_by_per(totalhp, percentage):
