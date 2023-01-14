efd22dc0e215a1ea52eca56757bfc373404ada63
ryuk04 <122408850+ryuk04@users.noreply.github.com>
2023-01-14 16:48:56 +0530

    Update owner.py

--- NekoRobot/modules/owner.py
+++ NekoRobot/modules/owner.py
@@ -2,9 +2,9 @@ from NekoRobot import pgram, OWNER_ID
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-button=InlineKeyboardMarkup([[InlineKeyboardButton(text="owner",url=f"https://t.me/JUMPMAN_BOY")],[InlineKeyboardButton(text="join here", url="https://t.me/EXTINCTION_XD")]])  
+button=InlineKeyboardMarkup([[InlineKeyboardButton(text="owner",url=f"https://t.me/JUMPMAN_BOY")],[InlineKeyboardButton(text="Join Here ", url="https://t.me/EXTINCTION_XD")]])  
        
 
 @pgram.on_message(filters.command("owner"))
 async def _owner(_, message):
-    await message.reply_text("here is my owner and join link",reply_markup=button)
+    await message.reply_text("Here is my Owner and My Owners Group Link",reply_markup=button)
