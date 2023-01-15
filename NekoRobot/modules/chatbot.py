058e212b59813ea5713c18e32bf5e895896acb84
ryuk04 <122408850+ryuk04@users.noreply.github.com>
2023-01-15 14:16:00 +0530

    Update chatbot.py

--- NekoRobot/modules/chatbot.py
+++ NekoRobot/modules/chatbot.py
@@ -1,150 +1,162 @@
-# Credits to MetaVoid Team for making this module.
-
-import json
 import html
-import requests
-from NekoRobot.modules.sql import log_channel_sql as logsql
-import NekoRobot.modules.mongo.chatbot_mongo as sql
-from NekoRobot import AI_API_KEY as api
-
+import json
+import re
 from time import sleep
-from telegram import ParseMode
-from telegram import (InlineKeyboardButton,
-                      InlineKeyboardMarkup, ParseMode, Update)
-from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler, Filters, MessageHandler)
+
+import requests
+from telegram import (
+    CallbackQuery,
+    Chat,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ParseMode,
+    Update,
+    User,
+)
+from telegram.ext import (
+    CallbackContext,
+    CallbackQueryHandler,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+    run_async,
+)
 from telegram.utils.helpers import mention_html
+
+import NekoRobot.modules.sql.kuki_sql as sql
+from NekoRobot import BOT_ID, BOT_NAME, BOT_USERNAME, NEKO_PTB
 from NekoRobot.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
-from NekoRobot import  NEKO_PTB
-from NekoRobot.modules.log_channel import gloggable, loggable
+from NekoRobot.modules.log_channel import gloggable
 
-bot_name = f"{NEKO_PTB.bot.first_name}"
 
+@run_async
 @user_admin_no_reply
-@loggable
 @gloggable
-def chatbot_status(update: Update, context: CallbackContext):
-    query= update.callback_query
-    bot = context.bot
-    user = update.effective_user
-    if query.data == "add_chatbot":
-        chat = update.effective_chat
-        is_chatbot = sql.is_chatbot(chat.id)
-        if not is_chatbot:
-            is_chatbot = sql.add_chatbot(chat.id)
-            LOG = (
+def asuxrm(update: Update, context: CallbackContext) -> str:
+    query: Optional[CallbackQuery] = update.callback_query
+    user: Optional[User] = update.effective_user
+    match = re.match(r"rm_chat\((.+?)\)", query.data)
+    if match:
+        user_id = match.group(1)
+        chat: Optional[Chat] = update.effective_chat
+        is_kuki = sql.set_kuki(chat.id)
+        if is_kuki:
+            is_kuki = sql.set_kuki(user_id)
+            return (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"AI_DISABLED\n"
+                f"<b>Admin :</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+            )
+        else:
+            update.effective_message.edit_text(
+                "{} ᴄʜᴀᴛʙᴏᴛ ᴅɪsᴀʙʟᴇᴅ ʙʏ {}.".format(
+                    NEKO_PTB.bot.first_name, mention_html(user.id, user.first_name)
+                ),
+                parse_mode=ParseMode.HTML,
+            )
+
+    return ""
+
+
+@run_async
+@user_admin_no_reply
+@gloggable
+def asuxadd(update: Update, context: CallbackContext) -> str:
+    query: Optional[CallbackQuery] = update.callback_query
+    user: Optional[User] = update.effective_user
+    match = re.match(r"add_chat\((.+?)\)", query.data)
+    if match:
+        user_id = match.group(1)
+        chat: Optional[Chat] = update.effective_chat
+        is_kuki = sql.rem_kuki(chat.id)
+        if is_kuki:
+            is_kuki = sql.rem_kuki(user_id)
+            return (
                 f"<b>{html.escape(chat.title)}:</b>\n"
                 f"AI_ENABLE\n"
-                f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
-            )
-            log_channel = logsql.get_chat_log_channel(chat.id)
-            if log_channel:
-                bot.send_message(
-                log_channel,
-                LOG,
-                parse_mode=ParseMode.HTML,
-                disable_web_page_preview=True,
-            )
-            update.effective_message.edit_text(
-                f"{bot_name} Chatbot Enabled by {mention_html(user.id, user.first_name)}.",
-                parse_mode=ParseMode.HTML,
-            )
-            return LOG
-        elif is_chatbot:
-            return update.effective_message.edit_text(
-                f"{bot_name} Chatbot Already Enabled.",
-                parse_mode=ParseMode.HTML,
+                f"<b>Admin :</b> {mention_html(user.id, html.escape(user.first_name))}\n"
             )
         else:
-            return update.effective_message.edit_text(
-                "Error!",
-                parse_mode=ParseMode.HTML,
-            )
-    elif query.data == "rem_chatbot":
-        chat = update.effective_chat
-        is_chatbot = sql.is_chatbot(chat.id)
-        if is_chatbot:
-            is_chatbot = sql.rm_chatbot(chat.id)
-            LOG = (
-                f"<b>{html.escape(chat.title)}:</b>\n"
-                f"AI_DISABLE\n"
-                f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
-            )
-            log_channel = logsql.get_chat_log_channel(chat.id)
-            if log_channel:
-                bot.send_message(
-                log_channel,
-                LOG,
-                parse_mode=ParseMode.HTML,
-                disable_web_page_preview=True,
-            )
             update.effective_message.edit_text(
-                f"{bot_name} Chatbot disabled by {mention_html(user.id, user.first_name)}.",
-                parse_mode=ParseMode.HTML,
-            )
-            return LOG
-        elif not is_chatbot:
-            return update.effective_message.edit_text(
-                f"{bot_name} Chatbot Already Disabled.",
-                parse_mode=ParseMode.HTML,
-            )
-        else:
-            return update.effective_message.edit_text(
-                "Error!",
+                "{} ᴄʜᴀᴛʙᴏᴛ ᴇɴᴀʙʟᴇᴅ ʙʏ {}.".format(
+                    NEKO_PTB.bot.first_name, mention_html(user.id, user.first_name)
+                ),
                 parse_mode=ParseMode.HTML,
             )
 
+    return ""
+
+
+@run_async
 @user_admin
-@loggable
-def chatbot(update: Update, context: CallbackContext):
+@gloggable
+def NekoRobotchatbot(update: Update, context: CallbackContext):
     message = update.effective_message
-    msg = "Choose an option"
-    keyboard = InlineKeyboardMarkup([[
-        InlineKeyboardButton(
-            text="Enable",
-            callback_data=r"add_chatbot")],
-       [
-        InlineKeyboardButton(
-            text="Disable",
-            callback_data=r"rem_chatbot")]])
+    msg = "• ᴄʜᴏᴏsᴇ ᴀɴ ᴏᴩᴛɪᴏɴ ᴛᴏ ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ ᴄʜᴀᴛʙᴏᴛ"
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="ᴇɴᴀʙʟᴇ", callback_data="add_chat({})"),
+                InlineKeyboardButton(text="ᴅɪsᴀʙʟᴇ", callback_data="rm_chat({})"),
+            ],
+        ]
+    )
     message.reply_text(
-        msg,
+        text=msg,
         reply_markup=keyboard,
         parse_mode=ParseMode.HTML,
     )
 
-def bot_message(context: CallbackContext, message):
+
+def kuki_message(context: CallbackContext, message):
     reply_message = message.reply_to_message
-    if reply_message:
-        if reply_message.from_user.id == context.bot.get_me().id:
+    if message.text.lower() == "kuki":
+        return True
+    elif BOT_USERNAME in message.text.upper():
+        return True
+    elif reply_message:
+        if reply_message.from_user.id == BOT_ID:
             return True
     else:
         return False
 
-def chatbot_msg(update: Update, context: CallbackContext):
+
+def chatbot(update: Update, context: CallbackContext):
     message = update.effective_message
     chat_id = update.effective_chat.id
     bot = context.bot
-    is_chatbot = sql.is_chatbot(chat_id)
-    if not is_chatbot:
+    is_kuki = sql.is_kuki(chat_id)
+    if is_kuki:
         return
-	
-    if message.text and not message.document:
-        if not bot_message(context, message):
-            return
-        Message = message.text
-        bot.send_chat_action(chat_id, action="typing")
-        chatbot = requests.get('https://itsprodev.cf/chatbot/SOME1HING.php?api=' + api + '&message=' + Message)
-        Chat = json.loads(chatbot.text)
-        Chat = Chat['reply']
-        sleep(0.3)
-        message.reply_text(Chat, timeout=60)
 
-CHATBOTK_HANDLER = CommandHandler("chatbot", chatbot, run_async = True)
-ADD_CHAT_HANDLER = CallbackQueryHandler(chatbot_status, pattern=r"add_chatbot", run_async = True)
-RM_CHAT_HANDLER = CallbackQueryHandler(chatbot_status, pattern=r"rem_chatbot", run_async = True)
+    if message.text and not message.document:
+        if not kuki_message(context, message):
+            return
+        bot.send_chat_action(chat_id, action="typing")
+        url = f"https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/{BOT_NAME}/Anonymous/message={message.text}"
+        request = requests.get(url)
+        results = json.loads(request.text)
+        sleep(0.5)
+        message.reply_text(results["reply"])
+
+
+__help__ = f"""
+*{BOT_NAME} has an chatbot whic provides you a seemingless chatting experience :*
+
+ »  /chatbot *:* Shows chatbot control panel
+"""
+
+__mod_name__ = "Chatbot"
+
+
+CHATBOTK_HANDLER = CommandHandler("chatbot", NekoRobotchatbot)
+ADD_CHAT_HANDLER = CallbackQueryHandler(asuxadd, pattern=r"add_chat")
+RM_CHAT_HANDLER = CallbackQueryHandler(asuxrm, pattern=r"rm_chat")
 CHATBOT_HANDLER = MessageHandler(
-    Filters.text & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!")
-                    & ~Filters.regex(r"^\/")), chatbot_msg, run_async = True)
+    Filters.text
+    & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!") & ~Filters.regex(r"^\/")),
+    chatbot,
+)
 
 NEKO_PTB.add_handler(ADD_CHAT_HANDLER)
 NEKO_PTB.add_handler(CHATBOTK_HANDLER)
